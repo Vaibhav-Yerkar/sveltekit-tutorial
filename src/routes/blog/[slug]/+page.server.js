1c493b5d997c09d6a896473b4ad2007ae64cfa99
Vaibhav-Yerkar <vaibhavyerkar2017@gmail.com>
2024-07-01 23:47:01 +0530

    added postgres

--- src/routes/blog/[slug]/+page.server.js
+++ src/routes/blog/[slug]/+page.server.js
@@ -1,3 +1,11 @@
+import { sql } from "@vercel/postgres";
+
+export async function load({ params, locals }) {
+  return {
+    content: await sql`SELECT * from NAMES where user_id='${locals.user}'`
+  }
+}
+
 export async function load({params}){
   
   return {
