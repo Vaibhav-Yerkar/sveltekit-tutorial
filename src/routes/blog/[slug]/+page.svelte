ddedc47679f0915ec06838eee85481b4c9cdf61d
Vaibhav-Yerkar <vaibhavyerkar2017@gmail.com>
2024-07-06 11:35:30 +0530

    postgres established

--- src/routes/blog/[slug]/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  export let data;
-</script>
-
-<h1 class="text-red-500 font-bold">{data.content}</h1>
--- src/routes/blog/list/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  import Table from '$lib/Table.svelte';
-</script>
-
-<h1>List of Blogs</h1>
-<Table />
