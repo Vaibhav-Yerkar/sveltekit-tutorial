c1f97bdcd1613eb50cccc12feaada01ff6112c6a
Vaibhav-Yerkar <vaibhavyerkar2017@gmail.com>
2024-07-17 12:50:33 +0530

    updated profile page UI

--- src/lib/Table.svelte
+++ src/lib/Table.svelte
@@ -65,7 +65,7 @@
 <div
 	class="mt-10 pt-10 w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg"
 >
-	<div class="flex items-center justify-between mb-4">
+	<div class="flex items-center justify-between my-8">
 		<div class="space-y-1">
 			<h2 class="text-xl font-semibold">List of Users</h2>
 			<p class="text-sm text-gray-500">
